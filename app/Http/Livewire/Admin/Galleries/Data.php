12624c806ac3d309b757c8ed21422ee7eb95b0ba
safna prasetiono <safnaprasetiono71@gmail.com>
2022-07-01 14:30:26 +0700

    update galleri sistem

--- app/Http/Livewire/Admin/Galleries/Data.php
+++ app/Http/Livewire/Admin/Galleries/Data.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Admin\Galleries;
 
+use App\Models\galleries;
 use App\Models\galleries_content;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -9,6 +10,27 @@ use Livewire\WithPagination;
 class Data extends Component
 {
     use WithPagination;
+    public $post_id;
+
+    protected $listeners = ["deleteAction" => "delete"];
+
+    public function removed($id)
+    {
+        $this->post_id = $id;
+        $this->dispatchBrowserEvent('deleteConfrimed');
+    }
+
+    public function delete()
+    {
+        $data = galleries_content::find($this->post_id);
+        if ($data) {
+            galleries::where('id_galleries_contents', $data->id_galleries_contents)->delete();
+            $data->delete();
+            $this->dispatchBrowserEvent('success', 'Data has been delete!');
+        } else {
+            $this->dispatchBrowserEvent('error', 'sorry something problem in database!');
+        }
+    }
     
     public function render()
     {
--- resources/views/livewire/admin/galleries/data.blade.php
+++ resources/views/livewire/admin/galleries/data.blade.php
@@ -1,15 +1,15 @@
 <div>
 	<div class="d-flex align-items-center py-3">
 		<a class="btn btn-theme px-3" href="{{ route('admin.gallery.create') }}">Tambah</a>
-        <div class="ms-auto">
-            <select wire:model='pages' class="form-select" aria-label="Default select example">
-                <option value="12">12</option>
-                <option value="25">25</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-                <option value="99999">all</option>
-              </select>
-        </div>
+		<div class="ms-auto">
+			<select wire:model='pages' class="form-select" aria-label="Default select example">
+				<option value="12">12</option>
+				<option value="25">25</option>
+				<option value="50">50</option>
+				<option value="100">100</option>
+				<option value="99999">all</option>
+			</select>
+		</div>
 	</div>
 	<div class="table-responsive mb-3">
 		<table class="table table-borderless">
@@ -34,9 +34,11 @@
 						<span class="badge rounded-pill bg-secondary px-3">Pasive</span>
 						@endif
 					</td>
-					<td>{{ date('d', strtotime($item->date_start)) }} - {{ date('d F Y', strtotime($item->date_end)) }}</td>
+					<td>{{ date('d', strtotime($item->date_start)) }} - {{ date('d F Y', strtotime($item->date_end)) }}
+					</td>
 					<td>
-						<a href="{{ route('admin.gallery.update', ['id' => $item->id_galleries_contents]) }}" class="btn btn-warning btn-sm text-white">
+						<a href="{{ route('admin.gallery.update', ['id' => $item->id_galleries_contents]) }}"
+							class="btn btn-warning btn-sm text-white">
 							<i class="fas fa-pencil-alt fa-sm fa-fw"></i>
 						</a>
 						<button wire:click='removed({{ $item->id_galleries_contents }})' class="btn btn-danger btn-sm">
@@ -48,23 +50,23 @@
 			</tbody>
 		</table>
 	</div>
-    <div class="d-flex align-items-center">
-        <p class="mb-0 border py-1 px-2 rounded">
-            <span class="fw-bold">{{ $data->count() }}</span>
-        </p>
-        @if ($data->hasPages())
-        <nav class="ms-auto">
-            {{ $data->links('admin.layouts.paginations') }}
-        </nav>
-        @endif
-    </div>
+	<div class="d-flex align-items-center">
+		<p class="mb-0 border py-1 px-2 rounded">
+			<span class="fw-bold">{{ $data->count() }}</span>
+		</p>
+		@if ($data->hasPages())
+		<nav class="ms-auto">
+			{{ $data->links('admin.layouts.paginations') }}
+		</nav>
+		@endif
+	</div>
 
 	<script src="{{ url('/dist/style/js/jquery.js') }}"></script>
 	<script>
 		document.addEventListener('deleteConfrimed', function() {
         Swal.fire({
                 title: "Apa anda yakin?",
-                text: "Menghapus data banner ini!!!",
+                text: "Menghapus data galleri ini!!!",
                 icon: "warning",
                 showCancelButton: true,
                 confirmButtonText: 'Yes, delete!',
@@ -84,26 +86,20 @@
 			$('#bannerModal').modal('hide');
 		});
 
+		window.addEventListener('success', event => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Good Jobs!',
+                text: event.detail,
+            })
+        })
+        window.addEventListener('erros', event => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Opps...!',
+                text: event.detail,
+            })
+        })
+
 	</script>
-	@if(session()->has('success'))
-	<script>
-		Swal.fire({
-			icon: 'success',
-			title: 'Good Jobs!',
-			text: '{{ session()->get("success") }}',
-			showConfirmButton: false,
-			timer: 2500
-		})
-	</script>
-	@elseif(session()->has('error'))
-	<script>
-		Swal.fire({
-			icon: 'error',
-			title: 'Opps...!',
-			text: '{{ session()->get("error") }}',
-			showConfirmButton: false,
-			timer: 2500
-		})
-	</script>
-	@endif
 </div>
